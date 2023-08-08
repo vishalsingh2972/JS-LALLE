31458a6302ff4d6b71b169b0d74d8392c05e6ba0
Vishal Singh <vishalsinghpanwar02@gmail.com>
2023-08-08 16:48:19 +0530

    updated

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const API_KEY = "add your OpenAI API key here";
 const submitIcon = document.querySelector("#submit-icon");
+const inputElement = document.querySelector("input");
 
-const getImages = () => {
+const getImages = async() => {
   const options = {
     method: "POST",
     headers: {
@@ -9,15 +10,23 @@ const getImages = () => {
       'Content-Type': 'application/json'
     },
     body: JSON.stringify({
-      "prompt": "", 
+      "prompt": inputElement.value, 
       "n": 4,
       "size": "1024x1024"
     })
-
   }
-  try{
-    fetch('https://api.openai.com/v1/images/completions', options);
 
+  try{
+    const response = await fetch('https://api.openai.com/v1/images/completions', options);
+    const data = await response.json();
+    console.log(data);
+
+    data?.data.forEach(image => {
+      const imageContainer = document.createElement('div');
+      
+    })
+
+    
   } catch (error){
     console.error(error); //or put console.log(error)
   }
